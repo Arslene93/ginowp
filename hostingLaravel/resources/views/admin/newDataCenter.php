c0b196c5fc2b576c56191a24980392703d00f466
Mohamed Zayani <zayanimed97@gmail.com>
2021-02-15 16:44:08 +0100

    public files

---
+++ hostingLaravel/resources/views/admin/newDataCenter.php
@@ -0,0 +1,154 @@
+
+<?php echo view('templates/header')?>
+
+<body id="kt_body" class="subheader-enabled aside-enabled aside-fixed aside-secondary-disabled">
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!--begin::Main-->
+<!--begin::Header Mobile-->
+<div id="kt_header_mobile" class="header-mobile">
+    <!--begin::Logo-->
+    <a href="/metronic/demo3/index.html">
+        <img alt="Logo" src="<?= base_url('template')?>/dist/assets/media/logos/logo-letter-2.png" class="logo-default max-h-30px">
+    </a>
+    <!--end::Logo-->
+    <!--begin::Toolbar-->
+    <div class="d-flex align-items-center">
+        <button class="btn p-0 burger-icon burger-icon-left" id="kt_aside_mobile_toggle">
+            <span></span>
+        </button>
+    </div>
+    <!--end::Toolbar-->
+</div>
+<!--end::Header Mobile-->
+<div class="d-flex flex-column flex-root">
+    <!--begin::Page-->
+    <div class="d-flex flex-row flex-column-fluid page">
+        <!--begin::Aside-->
+        <?php echo view('templates/aside.php')?>
+        <!--end::Aside-->
+        <!--begin::Wrapper-->
+        <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+            <!--begin::Content-->
+            <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                <!--begin::Subheader-->
+                <div class="subheader py-3 py-lg-8 subheader-transparent" id="kt_subheader">
+                    <div class="container d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+                        <!--begin::Info-->
+                        <div class="d-flex align-items-center mr-1">
+                            <!--begin::Page Heading-->
+                            <div class="d-flex align-items-baseline flex-wrap mr-5">
+                                <!--begin::Page Title-->
+                                <h2 class="d-flex align-items-center text-dark font-weight-bold my-1 mr-3">Layout Builder</h2>
+                                <!--end::Page Title-->
+                                <!--begin::Breadcrumb-->
+                                <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold my-2 p-0">
+                                    <li class="breadcrumb-item text-muted">
+                                        <a href="" class="text-muted">Builder</a>
+                                    </li>
+                                </ul>
+                                <!--end::Breadcrumb-->
+                            </div>
+                            <!--end::Page Heading-->
+                        </div>
+                        <!--end::Info-->
+                        <!--begin::Toolbar-->
+                        <div class="d-flex align-items-center flex-wrap">
+                            <!--begin::Button-->
+                            <a href="<?=base_url('admin/newuser')?>" class="btn button-color btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2">
+										<span class="svg-icon svg-icon-lg">
+											<!--begin::Svg Icon | path"<?= base_url('template')?>/dist/assets/media/svg/icons/Communication/Add-user.svg-->
+											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<polygon points="0 0 24 0 24 24 0 24"></polygon>
+													<path d="M18,8 L16,8 C15.4477153,8 15,7.55228475 15,7 C15,6.44771525 15.4477153,6 16,6 L18,6 L18,4 C18,3.44771525 18.4477153,3 19,3 C19.5522847,3 20,3.44771525 20,4 L20,6 L22,6 C22.5522847,6 23,6.44771525 23,7 C23,7.55228475 22.5522847,8 22,8 L20,8 L20,10 C20,10.5522847 19.5522847,11 19,11 C18.4477153,11 18,10.5522847 18,10 L18,8 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
+													<path d="M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero"></path>
+												</g>
+											</svg>
+                                            <!--end::Svg Icon-->
+										</span>
+                                <span class="d-none d-md-inline">New Member</span>
+                            </a>
+                            <!--end::Button-->
+                            <!--begin::Dropdown-->
+                            <a href="<?=base_url('logs/logout')?>" class="btn btn-primary btn-fixed-height font-weight-bold px-2 px-lg-5 mr-2" >
+                                <span class="d-none d-md-inline">Logout</span>
+                            </a>
+                            <!--end::Dropdown-->
+                            <!--begin::Button-->
+                            <a href="#" class="btn btn-primary btn-icon font-weight-bold" data-toggle="modal" data-target="#kt_chat_modal">
+										<span class="svg-icon svg-icon-lg">
+											<!--begin::Svg Icon | path"<?= base_url('template')?>/dist/assets/media/svg/icons/Communication/Group-chat.svg-->
+											<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<rect x="0" y="0" width="24" height="24"></rect>
+													<path d="M16,15.6315789 L16,12 C16,10.3431458 14.6568542,9 13,9 L6.16183229,9 L6.16183229,5.52631579 C6.16183229,4.13107011 7.29290239,3 8.68814808,3 L20.4776218,3 C21.8728674,3 23.0039375,4.13107011 23.0039375,5.52631579 L23.0039375,13.1052632 L23.0206157,17.786793 C23.0215995,18.0629336 22.7985408,18.2875874 22.5224001,18.2885711 C22.3891754,18.2890457 22.2612702,18.2363324 22.1670655,18.1421277 L19.6565168,15.6315789 L16,15.6315789 Z" fill="#000000"></path>
+													<path d="M1.98505595,18 L1.98505595,13 C1.98505595,11.8954305 2.88048645,11 3.98505595,11 L11.9850559,11 C13.0896254,11 13.9850559,11.8954305 13.9850559,13 L13.9850559,18 C13.9850559,19.1045695 13.0896254,20 11.9850559,20 L4.10078614,20 L2.85693427,21.1905292 C2.65744295,21.3814685 2.34093638,21.3745358 2.14999706,21.1750444 C2.06092565,21.0819836 2.01120804,20.958136 2.01120804,20.8293182 L2.01120804,18.32426 C1.99400175,18.2187196 1.98505595,18.1104045 1.98505595,18 Z M6.5,14 C6.22385763,14 6,14.2238576 6,14.5 C6,14.7761424 6.22385763,15 6.5,15 L11.5,15 C11.7761424,15 12,14.7761424 12,14.5 C12,14.2238576 11.7761424,14 11.5,14 L6.5,14 Z M9.5,16 C9.22385763,16 9,16.2238576 9,16.5 C9,16.7761424 9.22385763,17 9.5,17 L11.5,17 C11.7761424,17 12,16.7761424 12,16.5 C12,16.2238576 11.7761424,16 11.5,16 L9.5,16 Z" fill="#000000" opacity="0.3"></path>
+												</g>
+											</svg>
+                                            <!--end::Svg Icon-->
+										</span>
+                            </a>
+                            <!--end::Button-->
+                        </div>
+                        <!--end::Toolbar-->
+                    </div>
+                </div>
+                <!--begin::Entry-->
+                <div class="d-flex flex-column-fluid">
+                    <!--begin::Container-->
+                    <div class="container">
+                        <?php echo view('logs/newDataCenter.php')?>
+                    </div>
+                    <!--end::Container-->
+                </div>
+                <!--end::Entry-->
+            </div>
+            <!--end::Content-->
+            <!--begin::Footer-->
+            <!--doc: add "bg-white" class to have footer with solod background color-->
+            <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+                <!--begin::Container-->
+                <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
+                    <!--begin::Copyright-->
+                    <div class="text-dark order-2 order-md-1">
+                        <span class="text-muted font-weight-bold mr-2">2021©</span>
+                        <a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Keenthemes</a>
+                    </div>
+                    <!--end::Copyright-->
+                    <!--begin::Nav-->
+                    <div class="nav nav-dark order-1 order-md-2">
+                        <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pr-3 pl-0">About</a>
+                        <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link px-3">Team</a>
+                        <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pl-3 pr-0">Contact</a>
+                    </div>
+                    <!--end::Nav-->
+                </div>
+                <!--end::Container-->
+            </div>
+            <!--end::Footer-->
+        </div>
+        <!--end::Wrapper-->
+    </div>
+    <!--end::Page-->
+</div>
+<!--end::Main-->
+<!--begin::Quick Actions Panel-->
+
+<!--end::Quick Actions Panel-->
+<!-- begin::User Panel-->
+<?php echo view('templates/user_panel.php')?>
+<!-- end::User Panel-->
+
+<!--begin::Quick Panel-->
+<?php echo view('templates/quick_panel.php')?>
+<!--end::Quick Panel-->
+<!--begin::Chat Panel-->
+<?php echo view('templates/chat_panel.php')?>
+
+<!--end::Chat Panel-->
+
+<?php echo view('templates/footer')?>
