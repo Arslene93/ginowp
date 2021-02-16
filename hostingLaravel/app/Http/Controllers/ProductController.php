449d70b563d1e95f4ac9f9b99aee5a5b50bc20c0
Arsléne <sakly.arslene@gmail.com>
2021-02-16 17:23:39 +0100

    crud

---
+++ hostingLaravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Product;
+use App\Models\Datacenter;
+
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $product = Product::get()->toArray();
+        $data_center = Datacenter::select('id' , 'name')->get();
+
+        return view('products.index' , ['data_center'=> $data_center]);
+    }
+
+
+    public function create()
+    {
+        return view('product.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|unique:product|max:255',
+            'TVA' => 'required',
+            'description' => 'required',
+
+        ]);
+
+        $product = new Product();
+        $product->title = $request->title;
+        $product->TVA = $request->TVA;
+        $product->description = $request->description;
+        $product->status = '1';
+
+
+         $prod = $product->save();
+        return redirect()->route('products.index');
+    }
+
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+
+        return view('products.edit');
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|unique:product|max:255',
+            'TVA' => 'required',
+            'description' => 'required',
+            'status' => 'required'
+        ]);
+
+        $product = Product::find($id);
+        $product->title = $request->title;
+        $product->TVA = $request->TVA;
+        $product->status = $request->status;
+
+        $product->save();
+
+        return redirect()->route('products.index');
+    }
+
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        //$feature->features()->detach();
+
+        return back();
+    }
+}
---
+++ hostingLaravel/resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('backend.layouts.app')
+
+@section('title', 'Dashboard')
+
+@push('styles')
+
+@endpush
+
+
+@section('content')
+
+    <div class="block-header">
+        <h2>DASHBOARD</h2>
+    </div>
